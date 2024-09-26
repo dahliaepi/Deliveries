97de5046047c761d9ddb1308a785cd4d70493559
Dahlia <koriche@MacBook-Pro-de-Dahlia.local>
2024-09-26 13:22:28 +0200

    SCRIPT.JS

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = "<h1>Hello World!</h1>";
+// Affiche le texte sur la page de résultat
+document.body.innerHTML = "<h1>Welcome to the javascript pool!</h1><p>Have some fun and don't forget to bring a towel.</p>";
+
+// Affiche le texte dans la console
+console.log("Website launched!");
